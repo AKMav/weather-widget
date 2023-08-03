c0e9f6cd6cc8da931a2140dac520603cf16ed07e
Akmal.Mavrayev <akmal.mavrayev@axdev.cloud>
2023-08-03 15:54:03 +0500

    feat: add search-inpput, loader

--- src/features/SearchLocation/ui/SearchLocation.ce.vue
+++ src/features/SearchLocation/ui/SearchLocation.ce.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="search-location"></div>
+  <div class="search-location">
+    <span class="search-location__wrapper">
+      <SearchIcon class="search-location__icon"></SearchIcon>
+      <input class="search-location__input" />
+    </span>
+    <LoaderUI v-if="false" />
+    <button v-else class="search-location__apply-btn">Apply</button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SearchIcon from '@/shared/icons/SearchIcon'
+import LoaderUI from '@/shared/components/LoaderUI'
+</script>
 
 <style lang="scss">
 @import './style.scss';
